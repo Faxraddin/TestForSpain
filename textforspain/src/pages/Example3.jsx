6609c62f51d4dd407a8507f400dc684ee8f584c0
Faxraddin <reydivonay@gmail.com>
2024-04-16 19:20:10 +0400

    example 3

--- textforspain/src/pages/Example3.jsx
+++ textforspain/src/pages/Example3.jsx
@@ -1,8 +1,18 @@
-import React from "react"
+import { Canvas } from "@react-three/fiber"
+import React from "react";
+import { Apartment1 } from "../models/Apartment1";
+import { OrbitControls } from "@react-three/drei";
 
 const Example3 = () => {
   return (
-    <div>Example3</div>
+    <div className="relative top-100 h-screen w-full">
+      <Canvas camera={{ position: [-6.119053075084781, 8.164465050167506,1.0085519544005341] }}>
+        <Apartment1/>
+        <OrbitControls/>
+        <ambientLight intensity={5}/>
+      </Canvas>
+    </div>
+
   )
 }
 export default Example3
