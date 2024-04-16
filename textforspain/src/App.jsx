392055e6fe5e62ad88d68f30b38ce3df55a58832
Faxraddin <reydivonay@gmail.com>
2024-04-16 19:01:50 +0400

    routes

--- textforspain/src/App.jsx
+++ textforspain/src/App.jsx
@@ -3,6 +3,8 @@ import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 
 import Example1 from "./pages/Example1";
+import Example2 from "./pages/Example2";
+import Example3 from "./pages/Example3";
 
 function App() {
 
@@ -11,6 +13,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route path="/" element={<Example1 />} />
+            <Route path="/Example2" element={<Example2 />} />
+            <Route path="/Example3" element={<Example3 />} />
           </Route>
         </Routes>
     </>
--- textforspain/src/components/Navbar.jsx
+++ textforspain/src/components/Navbar.jsx
@@ -6,8 +6,8 @@ const Navbar = () => {
     <header className="w-full h-[100px] border flex justify-center items-center">
         <div className="w-[80%] flex-wrap gap-5 flex justify-between items-center">
             <Link className="p-3 py-2 w-[120px] border-2 border-black rounded-[10px] " to={'/'}>Example 1</Link>
-            <Link className="p-3 py-2 w-[120px] border-2 border-black rounded-[10px] " to={'/'}>Example 1</Link>
-            <Link className="p-3 py-2 w-[120px] border-2 border-black rounded-[10px] " to={'/'}>Example 1</Link>
+            <Link className="p-3 py-2 w-[120px] border-2 border-black rounded-[10px] " to={'/Example2'}>Example 2</Link>
+            <Link className="p-3 py-2 w-[120px] border-2 border-black rounded-[10px] " to={'/Example3'}>Example 3</Link>
         </div>
     </header>
   )
