305c303a70ed60d29a29fcf3999bbccd216cf3bf
Faxraddin <reydivonay@gmail.com>
2024-04-19 10:55:13 +0400

    +2 vectors

--- textforspain/src/pages/Example7.jsx
+++ textforspain/src/pages/Example7.jsx
@@ -4,15 +4,17 @@ import { OrbitControls } from "@react-three/drei";
 import { Vector3, BufferGeometry, BufferAttribute } from "three";
 
 const Example7 = () => {
-  const vector1 = new Vector3(2, 2, 2);
+  const vector1 = new Vector3(2, 1, 3);
   const vector2 = new Vector3(2, 2, 3);
+  const vector3 = new Vector3(2, 2, 4);
+  const vector4 = new Vector3(2, 2, 3.5);
 
   const lineRef = useRef();
 
   const geometry = new BufferGeometry();
   geometry.setAttribute(
     "position",
-    new BufferAttribute(new Float32Array([vector1.x, vector1.y, vector1.z, vector2.x, vector2.y, vector2.z]), 3)
+    new BufferAttribute(new Float32Array([vector1.x, vector1.y, vector1.z, vector2.x, vector2.y, vector2.z, vector3.x, vector3.y, vector3.z, vector4.x, vector4.y, vector4.z]), 3)
   );
 
   return (
@@ -31,6 +33,14 @@ const Example7 = () => {
           <meshStandardMaterial color="gray" />
         </mesh>
         <mesh position={vector2}>
+          <boxGeometry args={[0.2, 0.2, 0.2]} />
+          <meshStandardMaterial color="gray" />
+        </mesh>
+        <mesh position={vector3}>
+          <boxGeometry args={[0.2, 0.2, 0.2]} />
+          <meshStandardMaterial color="gray" />
+        </mesh>
+        <mesh position={vector4}>
           <boxGeometry args={[0.2, 0.2, 0.2]} />
           <meshStandardMaterial color="gray" />
         </mesh>
