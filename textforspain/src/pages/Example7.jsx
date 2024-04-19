dbc4b3113209b7c5b274d3ac77adffb959cc47c1
Faxraddin <reydivonay@gmail.com>
2024-04-19 13:33:37 +0400

    all

--- textforspain/src/pages/Example7.jsx
+++ textforspain/src/pages/Example7.jsx
@@ -7,14 +7,18 @@ const Example7 = () => {
   const vector1 = new Vector3(2, 1, 3);
   const vector2 = new Vector3(2, 2, 3);
   const vector3 = new Vector3(2, 2, 4);
-  const vector4 = new Vector3(2, 2, 3.5);
+  const vector4 = new Vector3(2, 2, 5);
+  const vector5 = new Vector3(2, 1, 5);
+  const vector6 = new Vector3(2, 0, 5);
+  const vector7 = new Vector3(2, 0, 4);
+  const vector8 = new Vector3(2, 0, 3);
 
   const lineRef = useRef();
 
   const geometry = new BufferGeometry();
   geometry.setAttribute(
     "position",
-    new BufferAttribute(new Float32Array([vector1.x, vector1.y, vector1.z, vector2.x, vector2.y, vector2.z, vector3.x, vector3.y, vector3.z, vector4.x, vector4.y, vector4.z]), 3)
+    new BufferAttribute(new Float32Array([vector1.x, vector1.y, vector1.z, vector2.x, vector2.y, vector2.z, vector3.x, vector3.y, vector3.z, vector4.x, vector4.y, vector4.z, vector5.x, vector5.y, vector5.z,vector6.x, vector6.y, vector6.z,vector7.x, vector7.y, vector7.z,,vector8.x, vector8.y, vector8.z]), 3)
   );
 
   return (
@@ -41,6 +45,22 @@ const Example7 = () => {
           <meshStandardMaterial color="gray" />
         </mesh>
         <mesh position={vector4}>
+          <boxGeometry args={[0.2, 0.2, 0.2]} />
+          <meshStandardMaterial color="gray" />
+        </mesh>
+        <mesh position={vector5}>
+          <boxGeometry args={[0.2, 0.2, 0.2]} />
+          <meshStandardMaterial color="gray" />
+        </mesh>
+        <mesh position={vector6}>
+          <boxGeometry args={[0.2, 0.2, 0.2]} />
+          <meshStandardMaterial color="gray" />
+        </mesh>
+        <mesh position={vector7}>
+          <boxGeometry args={[0.2, 0.2, 0.2]} />
+          <meshStandardMaterial color="gray" />
+        </mesh>
+        <mesh position={vector8}>
           <boxGeometry args={[0.2, 0.2, 0.2]} />
           <meshStandardMaterial color="gray" />
         </mesh>
