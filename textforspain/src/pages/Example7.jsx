28a0fef9aaa4ba7ee26334ad883f154daab2b576
Faxraddin <reydivonay@gmail.com>
2024-04-19 15:51:24 +0400

    progress

--- textforspain/src/pages/Example7.jsx
+++ textforspain/src/pages/Example7.jsx
@@ -2,9 +2,9 @@ import React, { useRef } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import { Vector3, BufferGeometry, BufferAttribute } from "three";
+import * as THREE from 'three'
 
 const Example7 = () => {
-  // Define vectors
   const vectors = [
     new Vector3(2, 1, 3),
     new Vector3(2, 2, 3),
@@ -18,14 +18,12 @@ const Example7 = () => {
 
   const lineRef = useRef();
 
-  // Function to generate vertices for lines
   const generateVertices = () => {
     const vertices = [];
     for (let i = 0; i < vectors.length - 1; i++) {
       vertices.push(vectors[i].x, vectors[i].y, vectors[i].z);
       vertices.push(vectors[i + 1].x, vectors[i + 1].y, vectors[i + 1].z);
     }
-    // Add line segment connecting last vector to first vector
     vertices.push(
       vectors[vectors.length - 1].x,
       vectors[vectors.length - 1].y,
@@ -35,9 +33,34 @@ const Example7 = () => {
     return new Float32Array(vertices);
   };
 
-  // Create BufferGeometry for lines
   const geometry = new BufferGeometry();
   geometry.setAttribute("position", new BufferAttribute(generateVertices(), 3));
+
+  // Function to calculate triangle vertices dynamically
+  const calculateTriangleVertices = () => {
+    const triangle1Vertices = [
+      vectors[1].x, vectors[1].y, vectors[1].z,
+      vectors[7].x, vectors[7].y, vectors[7].z,
+      vectors[3].x, vectors[3].y, vectors[3].z
+    ];
+    const triangle2Vertices = [
+      vectors[7].x, vectors[7].y, vectors[7].z,
+      vectors[3].x, vectors[3].y, vectors[3].z,
+      vectors[5].x, vectors[5].y, vectors[5].z
+    ];
+    return {
+      triangle1Vertices,
+      triangle2Vertices
+    };
+  };
+
+  const { triangle1Vertices, triangle2Vertices } = calculateTriangleVertices();
+
+  const triangle1Geometry = new BufferGeometry();
+  triangle1Geometry.setAttribute("position", new BufferAttribute(new Float32Array(triangle1Vertices), 3));
+
+  const triangle2Geometry = new BufferGeometry();
+  triangle2Geometry.setAttribute("position", new BufferAttribute(new Float32Array(triangle2Vertices), 3));
 
   return (
     <div className="relative top-100 h-screen w-full">
@@ -45,13 +68,21 @@ const Example7 = () => {
         <ambientLight intensity={0.5} />
         <OrbitControls />
 
-        {/* Render lines */}
         <line ref={lineRef}>
           <primitive object={geometry} />
           <lineBasicMaterial attach="material" color="red" />
         </line>
 
-        {/* Render cubes */}
+        <mesh>
+          <primitive object={triangle1Geometry} />
+          <meshBasicMaterial attach="material" color="green" transparent opacity={0.5} side={THREE.DoubleSide} />
+        </mesh>
+
+        <mesh>
+          <primitive object={triangle2Geometry} />
+          <meshBasicMaterial attach="material" color="yellow" transparent opacity={0.5} side={THREE.DoubleSide} />
+        </mesh>
+
         {vectors.map((vector, index) => (
           <mesh key={index} position={vector}>
             <boxGeometry args={[0.2, 0.2, 0.2]} />
